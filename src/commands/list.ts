0fbe37145030697f3e877802fc59c9630bf4e60e
in-ch <sxin2949@naver.com>
2024-08-06 16:54:12 +0900

    feat: add fixed message util (#2)

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -5,21 +5,37 @@ import { fileURLToPath } from "url";
 
 import boxedMessage from "@/src/utils/boxed-message";
 import tableMessage from "@/src/utils/table-message";
+import fixedMessage from "../utils/fixed-message";
 
 /**
  * @description This command will list all the tasks.
  */
-export const list = new Command().command("list").description("list all the tasks").action(async () => {
+export const list = new Command()
+  .command("list")
+  .description("list all the tasks")
+  .action(async () => {
     const filePath = fileURLToPath(import.meta.url);
     const fileContent = fs.readFileSync(filePath, "utf-8");
-    const commandDescriptionPattern = /command\("(.+?)"\)\.description\("(.+?)"\)/g;
+    const commandDescriptionPattern =
+      /command\("(.+?)"\)\.description\("(.+?)"\)/g;
     let match;
     const results = [];
     while ((match = commandDescriptionPattern.exec(fileContent)) !== null) {
-        const command = match[1];
-        const description = match[2];
-        results.push({ command, description });
+      const command = match[1];
+      const description = match[2];
+      results.push({ command, description });
     }
-    boxedMessage({ messages: ["Welcome to Inch's CLI", "Here is a list of commands."], borderColor: "cyan", align: "left" });
-    tableMessage({ data: results, borderColor: "lightBlack", textColor: "lightGreen" });
-});
+    boxedMessage({
+      messages: ["Welcome to Inch's CLI", "Here is a list of commands."],
+      borderColor: "cyan",
+      align: "left",
+    });
+    tableMessage({
+      data: results,
+      borderColor: "lightBlack",
+      textColor: "lightGreen",
+    });
+    fixedMessage({
+      message: "For more information, use the --help flag with the command.",
+    });
+  });
